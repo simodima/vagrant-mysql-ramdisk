c3c45ab98b96156781a58b5e2123e39eede715f3
toretto460 <toretto460@gmail.com>
2013-07-05 09:08:37 +0200

    Skip override mysql config if file already exists

--- my-cookbooks/mysql_extra/recipes/default.rb
+++ my-cookbooks/mysql_extra/recipes/default.rb
@@ -3,5 +3,6 @@ include_recipe "mysql"
 template "/etc/mysql/conf.d/override.cnf" do
   source "override.cnf.erb"
   mode 644
-    notifies :restart, "service[mysql]"
+  not_if {File.exists?("/etc/mysql/conf.d/override.cnf")}
+  notifies :restart, "service[mysql]"
 end
